bbaa8637c173503cf31ceeb7e9a716c279bff1a7
Satyaki34 <satyaki.mld111@gmail.com>
2024-04-05 22:20:07 +0530

    New commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let count =1;
 let html=`  <label for="production-rule">Enter Your Production-rule:
 <input type="text" id="production-rule" placeholder="Format: S=AB" required/>
 </label>`;
-let count=1;
-document.body.querySelector('button').addEventListener('click',()=>{
-    
-    document.getElementsByTagName('label')[count++].insertAdjacentHTML('afterend',`${html}`);
-    // console.log(count);
-})
+document.body.querySelector("button").addEventListener("click", () => {
+  document
+    .getElementsByTagName("label")
+    [count++].insertAdjacentHTML("afterend", `${html}`);
+//   console.log(count);
+});
