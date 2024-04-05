a5b885044d12e2b7ac0dc396e1d391dd803cf05e
Satyaki34 <satyaki.mld111@gmail.com>
2024-04-05 21:59:02 +0530

    Updated script

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let html=`  <label for="production-rule">Enter Your Production-rule:
 <input type="text" id="production-rule" placeholder="Format: S=AB" required/>
 </label>`;
+let count=1;
 document.body.querySelector('button').addEventListener('click',()=>{
-    document.getElementsByTagName('label')[1].insertAdjacentHTML('afterend',`${html}`)
+    
+    document.getElementsByTagName('label')[count++].insertAdjacentHTML('afterend',`${html}`);
+    // console.log(count);
 })
