3ee94202f7bffd2d668c9fda7dbda5cb914c5119
Satyaki34 <satyaki.mld111@gmail.com>
2024-04-06 21:21:31 +0530

    readjusting html css & js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let count =1;
-let html=`  <label for="production-rule">Enter Your Production-rule:
-<input type="text" id="production-rule" placeholder="Format: S=AB" required/>
-</label>`;
+
 document.body.querySelector("button").addEventListener("click", () => {
+  let html=`  <label for="production-rule${count}">Enter Your Production-rule:
+<input type="text" id="production-rule${count}" placeholder="Format: S=AB" required/>
+</label>`;
   document
     .getElementsByTagName("label")
     [count++].insertAdjacentHTML("afterend", `${html}`);
