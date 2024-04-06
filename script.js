29e368fb39bd9f39bbe59f0407eb49a72fdbdf32
Dragon4926 <deadeye.040104@gmail.com>
2024-04-06 20:06:43 +0530

    Added responsiveness on smaller screens

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let count =1;
-let html=`  <label for="production-rule">Enter Your Production-rule:
-<input type="text" id="production-rule" placeholder="Format: S=AB" required/>
-</label>`;
+let count = 1;
+
 document.body.querySelector("button").addEventListener("click", () => {
+  let html = `  <label for="production-rule">Rule ${count}:
+  <input type="text" id="production-rule" placeholder="Format: S=AB" required/>
+  </label>`;
   document
     .getElementsByTagName("label")
     [count++].insertAdjacentHTML("afterend", `${html}`);
-//   console.log(count);
+  console.log(count);
 });
